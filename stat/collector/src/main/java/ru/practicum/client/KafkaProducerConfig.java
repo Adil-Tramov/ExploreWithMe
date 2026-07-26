12f1049200b1e3ffee7008c289f42eb3d384ce41
Adil Tramov <adiltram420@gmail.com>
2026-07-26 03:54:17 +0300

    4-2

--- stat/collector/src/main/java/ru/practicum/client/KafkaProducerConfig.java
+++ stat/collector/src/main/java/ru/practicum/client/KafkaProducerConfig.java
@@ -1,14 +1,17 @@
 package ru.practicum.client;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.stereotype.Component;
+import ru.practicum.config.KafkaProducerProperties;
 
 import java.util.Properties;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class KafkaProducerConfig {
@@ -30,6 +33,7 @@ public class KafkaProducerConfig {
         config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 30000);
         config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
 
+        log.info("KafkaProducer создан с bootstrap.servers={}", properties.getBootstrapServers());
         return new KafkaProducer<>(config);
     }
 }
